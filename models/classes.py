56ee7cf5743f9c6de21d720eeda82fa68a47e2e3
Ross Napier <ross-napier@hotmail.com>
2021-11-05 14:16:20 +0000

    Classes class added

---
+++ models/classes.py
@@ -0,0 +1,7 @@
+class Classes:
+    def __init__(self, name, date, time, duration, id = None):
+        self.name = name
+        self.date = date
+        self.time = time
+        self.duration = duration
+        self.id = id
