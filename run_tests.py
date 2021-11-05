2fe6e5eda9f51c9e290bbfdfaf9fdc049ec599da
Ross Napier <ross-napier@hotmail.com>
2021-11-05 15:23:57 +0000

    Gym tests made and passed

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,7 @@
 import unittest
 from tests.member_test import TestMember
-# from tests.class_test import TestClass
-# from tests.gym_test import TestGym
+from tests.class_test import TestClass
+from tests.gym_test import TestGym
 
 if __name__ == '__main__':
     unittest.main()
