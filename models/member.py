d29abc7a86ef01a80ffe5fb28791fd8b84bfbb3c
Ross Napier <ross-napier@hotmail.com>
2021-11-05 14:13:56 +0000

    Member class created

---
+++ models/member.py
@@ -0,0 +1,7 @@
+class Member:
+    def __init__(self, first_name, second_name, address, phone_no, id = None):
+        self.first_name = first_name
+        self.second_name = second_name
+        self.address = address
+        self.phone_no = phone_no
+        self.id = id
