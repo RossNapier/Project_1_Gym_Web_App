3788913926d965f36abaa1acc9c6f6d64cf91085
Ross Napier <ross-napier@hotmail.com>
2021-11-05 14:24:53 +0000

    Gym class added

---
+++ models/gym.py
@@ -0,0 +1,6 @@
+class Gym:
+    def __init__(self, name, address, phone_no, id = None):
+        self.name = name
+        self.address = address
+        self.phone_no = phone_no
+        self.id = id
