9e2bfb743e6f6e022e6ada173b52dde7da3f18a0
Ross Napier <ross-napier@hotmail.com>
2021-11-06 12:31:51 +0000

    index.html linked as landing page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.register_blueprint(members_blueprint)
 
 @app.route('/')
 def home():
-    return "Hello world"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug = True)
--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -8,4 +8,4 @@ members_blueprint = Blueprint("members", __name__)
 @members_blueprint.route("/members")
 def members():
     members = member_repo.select_all()
-    return render_template("members/show.html")
+    return render_template("members/show.html", all_members = members)
