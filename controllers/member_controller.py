4bc890b278c6b208d7412c6239e7a41737465342
Ross Napier <ross-napier@hotmail.com>
2021-11-07 12:25:27 +0000

    New member form added and tested

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 import repositories.member_repository as member_repo
+from models.member import Member
 
 members_blueprint = Blueprint("members", __name__)
 
@@ -9,6 +10,15 @@ def members():
     members = member_repo.select_all()
     return render_template("members/show.html", all_members = members)
 
-@members_blueprint.route("/members/new")
+@members_blueprint.route("/members/new", methods = ['GET'])
 def new_member():
     return render_template("members/new.html")
+
+@members_blueprint.route("/members", methods = ['POST'])
+def create_member():
+    first_name = request.form['first_name']
+    second_name = request.form['second_name']
+    phone_no = request.form['phone_no']
+    new_member = Member(first_name, second_name, phone_no)
+    member_repo.save(new_member)
+    return redirect('/members')
