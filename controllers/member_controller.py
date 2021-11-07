e85759b0ea3182a88376040257f43423a5336767
Ross Napier <ross-napier@hotmail.com>
2021-11-07 10:34:02 +0000

    New member button and link working, form not yet made

--- controllers/member_controller.py
+++ controllers/member_controller.py
@@ -8,3 +8,7 @@ members_blueprint = Blueprint("members", __name__)
 def members():
     members = member_repo.select_all()
     return render_template("members/show.html", all_members = members)
+
+@members_blueprint.route("/members/new")
+def new_member():
+    return render_template("members/new.html")
