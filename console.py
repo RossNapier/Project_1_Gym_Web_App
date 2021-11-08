545d4ba44a6fbc09c9a5b1543fadba2148f8eee0
Ross Napier <ross-napier@hotmail.com>
2021-11-08 14:23:41 +0000

    assigning members to classes added and working

--- console.py
+++ console.py
@@ -11,10 +11,11 @@ import repositories.schedule_repository as schedule_repo
 member1 = Member("Steve", "Rogers", "0131 234 3544")
 member2 = Member("Peter", "Parker", "0131 564 4332")
 member3 = Member("Natasha", "Romanoff", "0131 221 3454")
-
+member4 = Member("Pepper", "Potts", "0131 678 3985")
 member_repo.save(member1)
 member_repo.save(member2)
 member_repo.save(member3)
+member_repo.save(member4)
 
 class1 = Class("HIIT", "13/11/21", "18.00", 30)
 class2 = Class("Boxing", "15/11/21", "20.00", 45)
--- repositories/schedule_repository.py
+++ repositories/schedule_repository.py
@@ -4,11 +4,19 @@ import repositories.member_repository as member_repo
 import repositories.class_repository as class_repo
 
 def save(booking):
-    sql = "INSERT INTO schedule (class_id, member_id) VALUES (%s, %s) RETURNING id"
-    values = [booking.gym_class.id, booking.member.id]
+    sql = "INSERT INTO schedule (member_id, class_id) VALUES (%s, %s) RETURNING id"
+    values = [booking.member.id, booking.gym_class.id]
     results = run_sql(sql, values)
     booking.id = results[0]['id']
     return booking
+
+def save_by_id(member_id, class_id):
+    sql = "INSERT INTO schedule (member_id, class_id) VALUES (%s, %s) RETURNING id"
+    values = member_id, class_id
+    results = run_sql(sql, values)
+    booking = results[0]['id']
+    return booking
+
 
 def select_all():
     schedule = []
@@ -18,6 +26,6 @@ def select_all():
     for row in results:
         member = member_repo.select(row['member_id'])
         gym_class = class_repo.select(row['class_id'])
-        gym = Schedule(member, gym_class, row['id'])
+        gym = Schedule(member, gym_class)
         schedule.append(gym)
     return schedule
